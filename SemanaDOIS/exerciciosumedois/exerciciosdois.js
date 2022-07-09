91602ffd786669d00f6a8df941024002b762baae
Gabriel Alexandre Ferraz <gblferrazrodrigues@gmail.com>
2022-07-09 19:05:43 -0300

    Exercicios 1 e 2 Semana Dois

---
+++ SemanaDOIS/exerciciosumedois/exerciciosdois.js
@@ -0,0 +1,4 @@
+var nome = prompt("Qual seu nome ?");
+var email = prompt("Qual seu E-mail ?");
+console.log("Seu nome é :" + " " + nome + " Seu e-mail é :"+ " " + email);
+document.write("Seu nome é :" + " " + nome + "Seu e-mail é :"+ " " + email);

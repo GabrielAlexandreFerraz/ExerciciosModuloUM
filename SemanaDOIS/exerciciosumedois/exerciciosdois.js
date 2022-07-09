a762477a55707e4cc6917b9d739e552674df6b41
Gabriel Alexandre Ferraz <gblferrazrodrigues@gmail.com>
2022-07-09 19:18:32 -0300

    Exercicios 1 e 2 Semana Dois com H4

--- SemanaDOIS/exerciciosumedois/exerciciosdois.js
+++ SemanaDOIS/exerciciosumedois/exerciciosdois.js
@@ -1,4 +1,4 @@
 var nome = prompt("Qual seu nome ?");
 var email = prompt("Qual seu E-mail ?");
 console.log("Seu nome é :" + " " + nome + " Seu e-mail é :"+ " " + email);
-document.write("Seu nome é :" + " " + nome + "Seu e-mail é :"+ " " + email);
+document.write("<h4>Seu nome é :" + " " + nome + "Seu e-mail é :"+ " " + email + "</h4");

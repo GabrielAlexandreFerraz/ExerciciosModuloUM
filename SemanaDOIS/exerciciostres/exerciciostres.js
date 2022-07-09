3cf111c6ddcd1619fc22ed7ca669f73bf1596619
Gabriel Alexandre Ferraz <gblferrazrodrigues@gmail.com>
2022-07-09 19:31:58 -0300

    Exercicio 3

---
+++ SemanaDOIS/exerciciostres/exerciciostres.js
@@ -0,0 +1,5 @@
+var n1 = parseFloat( prompt ("Digite um Numero para Somar"));
+var n2 = parseFloat( prompt ("Digite outro Numero"));
+var resultado = n1 + n2
+
+document.write("<h4>" + resultado + "</h4");

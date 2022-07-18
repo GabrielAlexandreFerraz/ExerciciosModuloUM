fb3c3ad0377bf476f4f899d97b9d539039cb2240
Gabriel Alexandre Ferraz <gblferrazrodrigues@gmail.com>
2022-07-18 07:56:17 -0300

    exercicio 3

---
+++ SemanaTRES/exerciciosquatro/exercicioquatro.js
@@ -0,0 +1,11 @@
+const txt1 = document.getElementsByClassName("txt1")
+txt1.innerHTML = "texto 1 de Teste"
+
+const txt2 = document.getElementsByClassName("txt2")
+txt2.innerHTML = "texto 2 de Teste"
+
+const txt3 = document.getElementsByClassName("txt3")
+txt3.innerHTML = "texto 3 de Teste"
+
+const txt4 = document.getElementsByClassName("txt4")
+txt4.innerHTML = "texto 4 de Teste"
---
+++ SemanaTRES/exerciciotres/exerciciotres.js
@@ -0,0 +1,13 @@
+var array = [1,2,3,4,5,6,7,8,9,10]
+function exercicioTres(array) {
+    let pares = 0;
+    let impares = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 0) {
+        pares++;
+      } else {
+        impares++;
+      }
+    }
+    return [pares, impares];
+  }

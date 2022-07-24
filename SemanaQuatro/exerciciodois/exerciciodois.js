0b3f6c0977dd7323e4a6131064dc3d5cb8fc7b4c
Gabriel Alexandre Ferraz <gblferrazrodrigues@gmail.com>
2022-07-24 13:21:18 -0300

    exercicio 2

---
+++ SemanaQuatro/exerciciodois/exerciciodois.js
@@ -0,0 +1,15 @@
+// Crie uma classe chamada Filme com os atributos nome, anoDeLancamento e diretor.
+// Fora do escopo dessa classe, instancie ela e preencha os campos acessando 
+// seus atributos por meio do ponto (.).
+
+class Filme{
+    nome;
+    anoDeLancamento;
+    diretor;
+}
+var novoFilme = new Filme();
+novoFilme.nome = "Tinanic"
+novoFilme.anoDeLancamento = 1999;
+novoFilme.diretor = "James Camerom"
+
+console.log(novoFilme);

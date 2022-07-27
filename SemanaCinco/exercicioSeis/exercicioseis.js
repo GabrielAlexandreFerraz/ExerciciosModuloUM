4bbb6711bf880bc2be07721a266eac70ec7ea81a
Gabriel Alexandre Ferraz <gblferrazrodrigues@gmail.com>
2022-07-27 14:48:08 -0300

    exercicio seis semana 05

---
+++ SemanaCinco/exercicioSeis/exercicioseis.js
@@ -0,0 +1,7 @@
+// Utilize o if ternário para construir uma função que 
+// recebe o número do dias e retorna se pertence 
+// a primeira ou a segunda quinzena
+
+array = [25]
+
+let ternarium = (array >= 1 && array <= 15)? console.log("Primeira Quinzena"): console.log("Segunda Quinzena")
